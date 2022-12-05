a9a5aaeedd67bb345679eb8459e87c0868c45c67
Justin Egan <justin.d.egan@gmail.com>
2022-12-05 15:10:05 -0500

    userlist and dummy data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+
+  let users = [
+    {name: "Justin", age: "38"},
+    {name: "Sarah", age: "32"}
+  ]
+
   return (
     <div>
       <AddUser />
+      <UsersList users={users} />
     </div>
   );
 }
